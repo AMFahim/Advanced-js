76929a040b1175096276e35cfa0640641c2eabfb
AMFahim <amfahim2019@gmail.com>
2021-02-02 17:25:37 +0600

    true vs falsy

---
+++ true-false.js
@@ -0,0 +1,24 @@
+
+//If value is undefine.It will be false
+//""
+//0
+//Null
+//undefine
+//NaN
+let name;
+console.log(name);
+if(name){
+  console.log("It is true");
+}
+else{
+console.log("It is false");
+}
+
+//If variable has any value(without 0. If is it not in a string). It will be true
+const name2 = "Fahim";
+if(name2){
+  console.log("It is true");
+}
+else{
+console.log("It is false");
+}
