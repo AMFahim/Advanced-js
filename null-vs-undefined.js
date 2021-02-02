dd564987e635ed318aac3dbddac42bc5d693449a
AMFahim <amfahim2019@gmail.com>
2021-02-02 21:40:51 +0600

    null vs undefined is cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+let Fahim;
+
+function add(num1, num2){
+ console.log(num1 + num2);
+  return;
+}
+const result = add(32, 33);
+console.log(result);
+
+function add(num1, num2){
+console.log(num1, num2);
+}
+const result2 = add(32);
+console.log(result2);
+
+const person = {name: "Fahim", Id: 2342 };
+console.log(person.phone);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [21, 34, 23, 43];
+console.log(ages[6]);
